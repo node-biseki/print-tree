5c639797d606248c820ded102c8acf84c904cf17
Hericken <syuma49xx@gmail.com>
2018-11-13 20:29:58 +0900

    fix last node is not displayed

--- printTree.c
+++ printTree.c
@@ -9,11 +9,11 @@ void printTree(int lv, int node)
     int buff   = -1;
     int preLF  = 0;
 
-    for (i = 0; i < N; i++)
-        if (parent[i] == node || i == N-1) {
+    for (i = 0; i <= N; i++)
+        if (i == N || parent[i] == node) {
             if (buff != -1)  {
-                if (i == N-1) dep_flag[lv] = 0;
-                else          dep_flag[lv] = 1;
+                if (i == N) dep_flag[lv] = 0;
+                else        dep_flag[lv] = 1;
                 if (indent) {
                     for (j = 0; j < lv; j++)
                         if (dep_flag[j]) printf(" |  ");
@@ -23,11 +23,9 @@ void printTree(int lv, int node)
                     printf("---%c", V[buff]);
                     indent = 1;
                 }
-                preLF = 0;
                 printTree(lv+1, buff);
                 preLF = 1;
             }
-            //最後はN-1が入るがそのbuffは無視される
             buff = i;
         }
     if (!preLF) printf("\n");
