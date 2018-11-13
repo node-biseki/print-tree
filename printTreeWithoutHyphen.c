b8a88d197e18b0a84130ad811a31adf240eaac50
Hericken <syuma49xx@gmail.com>
2018-11-13 20:39:22 +0900

    init

---
+++ printTreeWithoutHyphen.c
@@ -0,0 +1,20 @@
+void printTreeWithoutHyphen(int lv, int node)
+{
+    int i, j;
+    int indent = 0;
+    int preLF  = 0;
+
+    for (i = 0; i < N; i++) {
+        if (parent[i] == node) {
+            if (indent) {
+                for (j = 0; j < lv; j++) printf("  ");
+            } else {
+                indent = 1;
+            }
+            printf(" %c", V[i]);
+            printTreeWithoutHyphen(lv+1, i);
+            preLF=1;
+        }
+    }
+    if (!preLF) printf("\n");
+}
